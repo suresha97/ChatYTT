064ebed3015a6875cdf1e4cdb2dcfbd40d0ebae0
Alok Suresh <aloksuresh@MacBook-Pro.station>
2023-12-18 15:27:37 +0000

    fix: api url.

--- client/src/components/GetCurretnChat.tsx
+++ client/src/components/GetCurretnChat.tsx
@@ -32,7 +32,7 @@ function getCurrentChat({setCurrentChat, userInput, currentChat, setUserInput}: 
         })
     }
 
-    fetch("https://3dvunaz8l4.execute-api.eu-west-1.amazonaws.com/Prod/get-answer/", options).then(
+    fetch("https://ejrq1n5fl2.execute-api.eu-west-1.amazonaws.com/Prod/get-answer", options).then(
         (response) => response.json()
     ).then(
         (data) => {
