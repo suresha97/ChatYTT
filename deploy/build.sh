0c5e51dc11e518df08f6edc14dd23f2340a8ead1
Alok Suresh <aloksuresh@Aloks-MBP.station>
2023-12-20 08:58:32 +0000

    feat: distribute deployment.

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-rm -rf ./lambda_code_uri
-mkdir lambda_code_uri
-cd lambda_code_uri && mkdir chatytt/ && cd ..
-cp -r ../chatytt/. ./lambda_code_uri/chatytt
-cp -r ../server/. ./lambda_code_uri/server
+rm -rf ./code_uri
+mkdir code_uri
+cd code_uri && mkdir chatytt/ && cd ..
+
+cp -r ../chatytt/. ./code_uri/chatytt
+cp -r ../server/. ./code_uri/server
 
 poetry export --without-hashes -f requirements.txt --output requirements.txt
-mv requirements.txt ./lambda_code_uri/
+mv requirements.txt ./code_uri/
