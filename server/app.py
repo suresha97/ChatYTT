51efc722bf4ed2243d1c1bab873ed7116517e5ba
suresha97 <suresha@tcd.ie>
2023-12-21 18:23:43 +0000

    style: spacing.

--- server/app.py
+++ server/app.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from chatytt.chains import ConversationalQAChain
+from chatytt.vector_store.pinecone_db import PineconeDB
 from server.utils.chat import parse_chat_history
 from server.utils.dynamodb import (
     is_new_user,
@@ -15,8 +17,8 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-# pinecone_db = PineconeDB(index_name="youtube-transcripts", embedding_source="open-ai")
-# chain = ConversationalQAChain(vector_store=pinecone_db.vector_store)
+pinecone_db = PineconeDB(index_name="youtube-transcripts", embedding_source="open-ai")
+chain = ConversationalQAChain(vector_store=pinecone_db.vector_store)
 
 
 @app.route("/get-answer/", methods=["POST"])
@@ -27,24 +29,20 @@ def get_answer():
         raw_chat_history = data["chatHistory"]
 
     chat_history = parse_chat_history(raw_chat_history)
+
     app.logger.debug(f"Query: {query}")
     app.logger.debug(f"Raw chat history: {raw_chat_history}")
     app.logger.debug(f"Parsed chat history: {chat_history}")
-    # response = chain.get_response(query=query, context=chat_history)
 
-    response = (
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra, nunc et cursus iaculis, lectus"
-        " orci ornare dui, sed euismod eros massa at purus."
-    )
-    import time
+    response = chain.get_response(query=query, context=chat_history)
 
-    time.sleep(1)
     return jsonify({"response": response})
 
 
 @app.route("/get-chat-history/", methods=["GET"])
 def get_chat_history():
     user_id = request.args.get("userId")
+    app.logger.debug(f"userid: {user_id}")
 
     if is_new_user(user_id):
         response = {"chatHistory": []}
@@ -62,6 +60,7 @@ def save_chat_history():
     app.logger.debug(f"request: {request.get_json()}")
     data = request.get_json()
     user_id = data["userId"]
+
     if is_new_user(user_id):
         create_chat_history(user_id=user_id, chat_history=data["chatHistory"])
     else:
