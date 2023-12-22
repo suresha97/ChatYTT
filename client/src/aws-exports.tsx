b8a6758b5a157bfaf9da887ae26afc12511fc905
suresha97 <suresha@tcd.ie>
2023-12-22 17:45:05 +0000

    fix: api stack name.

--- client/src/aws-exports.tsx
+++ client/src/aws-exports.tsx
@@ -1,8 +1,8 @@
 const awsExports = {
     "aws_project_region": "eu-west-1",
     "aws_cognito_region": "ew-west-1",
-    "aws_user_pools_id": "eu-west-1_bnayeg25r",
-    "aws_user_pools_web_client_id": "2k4bu2ttsll3nj7ou7gk2lb16",
+    "aws_user_pools_id": "eu-west-1_mLhvdIXVR",
+    "aws_user_pools_web_client_id": "3otklii2ogv5jjn5fl21ubv73h",
     "oauth": {}
 }
 
