e0c19d386a61613411dfbcafdc1be6a355e01b0e
suresha97 <suresha@tcd.ie>
2024-02-24 11:05:45 +0000

    feat: signout button.

---
+++ client/src/components/SignOutButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {chatItem} from "./LiveChatFeed";
+import signOutImg from "../assets/icons8-sign-out-50.png"
+
+interface Props {
+    signOut: Any
+}
+
+
+function SignOutButton({signOut}: Props) {
+    return (
+        <button
+            className="sign-out-button"
+            onClick={signOut}
+        > <img className="sign-out-img" src={signOutImg}/> Sign Out
+        </button>
+    )
+}
+
+export default SignOutButton
